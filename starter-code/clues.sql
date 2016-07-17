2c4bd22776094df931efadc9313402b09ea6c185
Indigo253931 <indigo253931@gmail.com>
2016-07-17 14:14:31 -0600

    query results

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -4,11 +4,23 @@
 
 SELECT * FROM country WHERE region='Southern Europe' ORDER BY population ASC;
 
+code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy |    gnp     |   gnpold   |            localname            |        governmentform         |     headofstate      | capital | code2 
+------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------------+------------+----------------------------
+----+-------------------------------+----------------------+---------+-------
+ VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                |       9.00 |            | Santa Sede/Cittï¿½ del Vaticano | Independent Church State      | Johannes Paavali II  |    3538 | VA
+
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
 SELECT * FROM countrylanguage WHERE countrycode='VAT';
+
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ VAT         | Italian  | t          |          0
+(1 row)
+
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
@@ -16,11 +28,20 @@ SELECT * FROM countrylanguage WHERE countrycode='VAT';
 
 SELECT * FROM countrylanguage WHERE language='Italian';
 
+ countrycode | language | isofficial | percentage 
+-------------+----------+------------+------------
+ SMR         | Italian  | t          |        100
+
+
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 SELECT * FROM city WHERE countrycode='SMR';
+
+  id  |    name    | countrycode |     district      | population 
+------+------------+-------------+-------------------+------------
+ 3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
@@ -29,11 +50,30 @@ SELECT * FROM city WHERE countrycode='SMR';
 SELECT * FROM city WHERE name LIKE 'Serra';
 SELECT * FROM country WHERE code='BRA';
 
+
+ id  | name  | countrycode |     district     | population 
+-----+-------+-------------+------------------+------------
+ 265 | Serra | BRA         | Espï¿½rito Santo |     302666
+(1 row)
+
+
+ code |  name  |   continent   |    region     | surfacearea | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2 
+------+--------+---------------+---------------+-------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------
+ BRA  | Brazil | South America | South America |  8.5474e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR
+(1 row)
+
+
+
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
  
 SELECT * FROM city WHERE id = '211';
+
+ id  |    name    | countrycode |     district     | population 
+-----+------------+-------------+------------------+------------
+ 211 | Brasï¿½lia | BRA         | Distrito Federal |    1969868
+(1 row)
 
 -- She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -54,4 +94,10 @@ SELECT * FROM city WHERE id = '211';
 
 SELECT * FROM city WHERE population=91084;
 
--- She's in ____________________________!
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
+(1 row)
+
+
+-- She's in Santa Monica, CA!
